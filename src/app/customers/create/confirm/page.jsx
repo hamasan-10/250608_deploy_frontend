5c3fb8b5edf4f678d3cb5e6086f2a38e1137cc0e
hamasan-10 <beach0304.m@gmail.com>
2025-06-08 21:42:16 +0900

    confirm test

--- src/app/customers/create/confirm/page.jsx
+++ src/app/customers/create/confirm/page.jsx
@@ -1,34 +1,49 @@
-"use client";
-export const dynamic = 'force-dynamic'; // ← ★これを追加する
-import OneCustomerInfoCard from "@/app/components/one_customer_info_card.jsx";
-import fetchCustomer from "./fetchCustomer";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+// page.jsx (デバッグ用)
+
+'use client';
+export const dynamic = 'force-dynamic';
 
 export default function ConfirmPage() {
-  const router = useRouter();
-  const customer_id = useSearchParams().get("customer_id");
-  const [customer, setCustomer] = useState(null);
-
-  useEffect(() => {
-    const fetchAndSetCustomer = async () => {
-      const customerData = await fetchCustomer(customer_id);
-      setCustomer(customerData);
-    };
-    fetchAndSetCustomer();
-  }, []);
-
   return (
-    <>
-      <div className="card bordered bg-white border-blue-200 border-2 max-w-sm m-4">
-        <div className="alert alert-success p-4 text-center">
-          正常に作成しました
-        </div>
-        <OneCustomerInfoCard {...customer} />
-        <button onClick={() => router.push("./../../customers")}>
-          <div className="btn btn-primary m-4 text-2xl">戻る</div>
-        </button>
-      </div>
-    </>
+    <div>
+      <h1>Confirm Page</h1>
+      <p>Build test successful.</p>
+    </div>
   );
 }
+
+
+// "use client";
+// export const dynamic = 'force-dynamic'; // ← ★これを追加する
+// import OneCustomerInfoCard from "@/app/components/one_customer_info_card.jsx";
+// import fetchCustomer from "./fetchCustomer";
+// import { useRouter, useSearchParams } from "next/navigation";
+// import { useEffect, useState } from "react";
+
+// export default function ConfirmPage() {
+//   const router = useRouter();
+//   const customer_id = useSearchParams().get("customer_id");
+//   const [customer, setCustomer] = useState(null);
+
+//   useEffect(() => {
+//     const fetchAndSetCustomer = async () => {
+//       const customerData = await fetchCustomer(customer_id);
+//       setCustomer(customerData);
+//     };
+//     fetchAndSetCustomer();
+//   }, []);
+
+//   return (
+//     <>
+//       <div className="card bordered bg-white border-blue-200 border-2 max-w-sm m-4">
+//         <div className="alert alert-success p-4 text-center">
+//           正常に作成しました
+//         </div>
+//         <OneCustomerInfoCard {...customer} />
+//         <button onClick={() => router.push("./../../customers")}>
+//           <div className="btn btn-primary m-4 text-2xl">戻る</div>
+//         </button>
+//       </div>
+//     </>
+//   );
+// }
