ff538264ad3b6ee2073534673a69c5e8ca66b538
hamasan-10 <beach0304.m@gmail.com>
2025-06-08 21:52:34 +0900

    config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
+// next.config.js
+
 require('dotenv').config()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
         // Reference a variable that was defined in the .env file and make it available at Build Time
         API_ENDPOINT: process.env.API_ENDPOINT,
-      },
+    },
+    output: 'standalone', // ← ★この行を追加します
 }
 
 module.exports = nextConfig
